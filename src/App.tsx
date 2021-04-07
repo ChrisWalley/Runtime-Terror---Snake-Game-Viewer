44253ed9cef7f0dfea30915f79728b85bd7e1df8
ChrisWalley <Chriswalley6@gmail.com>
2021-04-07 19:33:57 +0200

    Added parsing of gamestate information
    Completed viewer to display gamestate information as the SnakeAI server would present it.
    
    Fixed communication between server and client

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,165 @@
 import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
 
-import ClientComponent from "./ClientComponent.js";
+//import ClientComponent from "./ClientComponent.js";
 
-
-//const app = require('express')();
-//const http = require('http').Server(app);
-//const io = require('socket.io')(http);
-
+const ENDPOINT = "http://walleyco.de:3001";
 
 const blockSize = 10;
 const gridSize = 50;
 const startX = 10;
 const startY = 10;
-var currPos = 0;
+
+var currPos = -1;
+
+var applePos = "0 0";
+
+var snake0 = "";
+var snake1 = "";
+var snake2 = "";
+var snake3 = "";
+
+var obs0 = "";
+var obs1 = "";
+var obs2 = "";
 
 var viewerContext;
 
 function loop() {
-  var loopX;
-  var loopY;
-
-  var currX = ~~(currPos%gridSize);
-  var currY = ~~(currPos/gridSize);
-
-
-  viewerContext.fillStyle = 'rgb(255, 255, 255)';     //Clears area to white
-  viewerContext.fillRect(startX,startY, gridSize*blockSize, gridSize*blockSize);
-  viewerContext.fillRect(startX,startY + gridSize*blockSize + 20, gridSize*blockSize, 10);
-
-  viewerContext.fillStyle = 'rgb(0, 255, 0)';         //Draws progress bar at bottom
-
-  viewerContext.fillRect(startX, startY + gridSize*blockSize + 20, (currPos/(gridSize*gridSize))*(gridSize*blockSize), blockSize);
-
-
-  viewerContext.strokeStyle = 'rgb(0, 0, 0)';         //Draws square around viewer and progress bar
-  viewerContext.strokeRect(startX,startY, gridSize*blockSize, gridSize*blockSize);
-  viewerContext.strokeRect(startX,startY + gridSize*blockSize + 20, gridSize*blockSize, 10);
-
-  viewerContext.fillStyle = 'rgb(128,0,128)';
-  viewerContext.fillRect(startX + currX*blockSize, startY + currY*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
-
-
-  viewerContext.strokeStyle = 'rgb(125, 125, 125)';
-
-  for(loopX = 0; loopX < gridSize; loopX++)     //Draws squares around cells
+  if(currPos>=0)
   {
-    for(loopY = 0; loopY < gridSize; loopY++)
+    var loopX;
+    var loopY;
+
+    var currX = ~~(currPos%gridSize);
+    var currY = ~~(currPos/gridSize);
+
+
+    viewerContext.fillStyle = 'rgb(255, 255, 255)';     //Clears area to white
+    viewerContext.fillRect(startX,startY, gridSize*blockSize, gridSize*blockSize);
+    viewerContext.fillRect(startX,startY + gridSize*blockSize + 20, gridSize*blockSize, 10);
+
+    viewerContext.fillStyle = 'rgb(0, 255, 0)';         //Draws progress bar at bottom
+
+    viewerContext.fillRect(startX, startY + gridSize*blockSize + 20, (currPos/(gridSize*gridSize))*(gridSize*blockSize), blockSize);
+
+
+    viewerContext.strokeStyle = 'rgb(0, 0, 0)';         //Draws square around viewer and progress bar
+    viewerContext.strokeRect(startX,startY, gridSize*blockSize, gridSize*blockSize);
+    viewerContext.strokeRect(startX,startY + gridSize*blockSize + 20, gridSize*blockSize, 10);
+
+
+
+
+    viewerContext.strokeStyle = 'rgb(185, 185, 185)';
+
+    for(loopX = 0; loopX < gridSize; loopX++)     //Draws squares around cells
     {
-      viewerContext.strokeRect(startX + loopX*blockSize, startY + loopY*blockSize, blockSize, blockSize);
+      for(loopY = 0; loopY < gridSize; loopY++)
+      {
+        viewerContext.strokeRect(startX + loopX*blockSize, startY + loopY*blockSize, blockSize, blockSize);
+      }
     }
+
+
+    //Apple
+    var appleCoords = applePos.split(' ');
+    if(appleCoords.length > 1)
+    {
+      var appleX = parseInt(appleCoords[0]);
+      var appleY = parseInt(appleCoords[1]);
+      viewerContext.fillStyle = 'rgb(218,165,32)';
+      viewerContext.fillRect(startX + appleX*blockSize, startY +  appleY*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+
+    //Obstacles
+    viewerContext.fillStyle = 'rgb(108,108,108)';
+    var obsArr;
+    var i;
+
+    //Obstacle 1
+    obsArr = obs0.split(' ');
+    for (i = 1; i < obsArr.length; i++) {
+      var pos = obsArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+    //Obstacle 2
+    obsArr = obs1.split(' ');
+    for (i = 1; i < obsArr.length; i++) {
+      var pos = obsArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+    //Obstacle 3
+    obsArr = obs2.split(' ');
+    for (i = 1; i < obsArr.length; i++) {
+      var pos = obsArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+    //Snakes
+    var snakeArr;
+    var i;
+
+    //Snake 1
+    viewerContext.fillStyle = 'rgb(208,0,108)';
+
+    snakeArr = snake0.split(' ');
+    for (i = 4; i < snakeArr.length; i++) {
+      var pos = snakeArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+    //Snake 2
+    viewerContext.fillStyle = 'rgb(108,50,108)';
+
+    snakeArr = snake1.split(' ');
+    for (i = 4; i < snakeArr.length; i++) {
+      var pos = snakeArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+    //Snake 3
+    viewerContext.fillStyle = 'rgb(20,0,100)';
+
+    snakeArr = snake2.split(' ');
+    for (i = 4; i < snakeArr.length; i++) {
+      var pos = snakeArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+    //Snake 4
+    viewerContext.fillStyle = 'rgb(99,205,108)';
+
+    snakeArr = snake3.split(' ');
+    for (i = 4; i < snakeArr.length; i++) {
+      var pos = snakeArr[i];
+      var posArr = pos.split(',');
+      viewerContext.fillRect(startX + posArr[0]*blockSize, startY + posArr[1]*blockSize, blockSize, blockSize); //Draws coloured sqaure in viewer
+    }
+
+
+
+
+
+
   }
 
-
-
-  currPos+=.15;
-  currPos = currPos%(gridSize*gridSize);
   requestAnimationFrame(loop);
 }
 
 function App() {
   const viewerRef = React.useRef<HTMLCanvasElement>(null);
   const [context, setContext] = React.useState<CanvasRenderingContext2D | null>(null);
+  const [response, setResponse] = useState("Connecting...");
 
     useEffect(() => {
 
@@ -77,6 +176,49 @@ function App() {
         viewerContext = context;
         loop();
       }
+
+      const socket = socketIOClient(ENDPOINT);
+      socket.on("gamestate", data => {
+        setResponse("Gamestate "+data);
+        currPos = data;
+      });
+
+      socket.on("apple", data => {
+        applePos = data;
+      });
+
+      socket.on("snake", data => {
+        var arr = data.split(' ');
+        switch(arr[0]) {
+          case "0":
+              snake0 = data;
+            break;
+          case "1":
+              snake1 = data;
+            break;
+          case "2":
+              snake2 = data;
+            break;
+          case "3":
+              snake3 = data;
+          break;
+              }
+          });
+
+      socket.on("obstacle", data => {
+        var arr = data.split(' ');
+        switch(arr[0]) {
+          case "0":
+              obs0 = data;
+            break;
+          case "1":
+              obs1 = data;
+            break;
+          case "2":
+              obs2 = data;
+            break;
+              }
+          });
 
 
     }, [context]);
@@ -100,7 +242,33 @@ function App() {
           marginTop: 10,
         }}
       ></canvas>
-      <ClientComponent />
+      <p>
+      <time dateTime={response}>{response}</time>
+      </p>
+      <p>
+      <time dateTime={applePos}>{"Apple: "+applePos}</time>
+      </p>
+      <p>
+      <time dateTime={snake0}>{"Snake 1: "+snake0}</time>
+      </p>
+      <p>
+      <time dateTime={snake1}>{"Snake 2: "+snake1}</time>
+      </p>
+      <p>
+      <time dateTime={snake2}>{"Snake 3: "+snake2}</time>
+      </p>
+      <p>
+      <time dateTime={snake3}>{"Snake 4: "+snake3}</time>
+      </p>
+      <p>
+      <time dateTime={obs0}>{"Obstacle 1: "+obs0}</time>
+      </p>
+      <p>
+      <time dateTime={obs1}>{"Obstacle 2: "+obs1}</time>
+      </p>
+      <p>
+      <time dateTime={obs2}>{"Obstacle 3: "+obs2}</time>
+      </p>
     </div>
 
 
