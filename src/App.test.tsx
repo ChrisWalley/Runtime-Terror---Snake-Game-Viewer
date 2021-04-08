88ffa1ebe3862201ed487ed438318fddf8a585b5
ChrisWalley <Chriswalley6@gmail.com>
2021-04-08 18:07:34 +0200

    Added jest tsx integration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('test header', () => {
-  render(<App />);
+
+  act(() => {
+    render(<App />);
+  });
   const linkElement = screen.getByText(/Snake Game/i);
+
+
   expect(linkElement).toBeInTheDocument();
 });
