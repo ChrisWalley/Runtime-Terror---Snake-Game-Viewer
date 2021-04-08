b5945af863bf9163608b5c6db21b43ba6770ebf6
ChrisWalley <Chriswalley6@gmail.com>
2021-04-08 18:41:00 +0200

    Fixed typescript parsing in jest, and handled client disconnect

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils'
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('test header', () => {
