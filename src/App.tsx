a255579999139841bd745ce43566d17a121a2f1a
ChrisWalley <1846871@students.wits.ac.za>
2021-05-02 10:46:44 +0200

    Added viewer button interaction, redid progress bar and fixed tests

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ var gameState;
 var gameColours =
 {
   background: 'rgb(255, 255, 255)',
-  progressBarGreen: 'rgb(0, 255, 0)',
+  progressBarGreen: 'rgb(139, 195, 74)',
   progressBarBlue: 'rgb(0, 0, 255)',
   progressBarRed: 'rgb(255, 0, 0)',
   border: 'rgb(0, 0, 0)',
@@ -84,6 +84,9 @@ var progBar = {
   height:0
 }
 
+var imageObj1 = new Image();
+imageObj1.src = 'https://www.walleyco.de/snake.png'
+
 var viewerX = 0;
 var viewerY = 0;
 
@@ -100,26 +103,27 @@ function drawGameboard() {
     if(gameState!=null && gameState.state>=0)
     {
 
+      progBar =
+      {
+        x:startX,
+        y:startY + config.game_height*blockSize + 20,
+        width:(realtimeGamestate/(config.game_height*config.game_height))*(config.game_height*blockSize),
+        height:blockSize+3
+      }
         viewerContext.fillStyle = gameColours.progressBarGreen;         //Draws progress bar at bottom - current position
-        viewerContext.fillRect(startX, startY + config.game_width*blockSize + 20, (currentGamestate/(config.game_width*config.game_height))*(config.game_width*blockSize), blockSize);
+        viewerContext.fillRect(startX, startY + config.game_width*blockSize + 20, (currentGamestate/(config.game_width*config.game_height))*(config.game_width*blockSize), progBar.height);
 
         viewerContext.fillStyle = gameColours.progressBarBlue;         //Draws progress bar at bottom - real gametime
-        viewerContext.fillRect(startX + (realtimeGamestate/(config.game_width*config.game_width))*(config.game_height*blockSize) -2 , startY + config.game_height*blockSize + 20, 4, blockSize);
+        viewerContext.fillRect(startX + (realtimeGamestate/(config.game_width*config.game_width))*(config.game_height*blockSize) -2 , startY + config.game_height*blockSize + 20, 4, progBar.height);
 
         viewerContext.fillStyle = gameColours.progressBarRed;         //Draws progress bar at bottom - started viewing gametime
-        viewerContext.fillRect(startX + (startedViewingGamestate/(config.game_width*config.game_width))*(config.game_height*blockSize) -2, startY + config.game_height*blockSize + 20, 4, blockSize);
+        viewerContext.fillRect(startX + (startedViewingGamestate/(config.game_width*config.game_width))*(config.game_height*blockSize) -2, startY + config.game_height*blockSize + 20, 4, progBar.height);
 
-        progBar =
-        {
-          x:startX,
-          y:startY + config.game_height*blockSize + 20,
-          width:(realtimeGamestate/(config.game_height*config.game_height))*(config.game_height*blockSize),
-          height:blockSize
-        }
+
 
         viewerContext.strokeStyle = gameColours.border;          //Draws square around viewer and progress bar
         viewerContext.strokeRect(startX,startY, config.game_width*blockSize, config.game_height*blockSize);
-        viewerContext.strokeRect(startX,startY + config.game_height*blockSize + 20, config.game_width*blockSize, 10);
+        viewerContext.strokeRect(startX,startY + config.game_height*blockSize + 20, config.game_width*blockSize, progBar.height);
 
 
         if(gameDrawCells)
@@ -206,6 +210,8 @@ function drawGameboard() {
         for (i = 0; i < snakeRects.length; i++) {
           viewerContext.fillRect(startX + snakeRects[i]['startX']*blockSize, startY + snakeRects[i]['startY']*blockSize, snakeRects[i]['width']*blockSize, snakeRects[i]['height']*blockSize); //Draws coloured sqaure in viewer
         }
+        viewerContext.drawImage(imageObj1,progBar.x+(currentGamestate/(config.game_height*config.game_height))*(config.game_height*blockSize)-3,progBar.y-2);
+
     }
     else if (lastGameRef>=0){
       var col1X = 100;
@@ -227,7 +233,6 @@ function drawGameboard() {
 
       viewerContext.fillText("Snake 4", col1X, colStartY+350);
       viewerContext.fillText(lastGameSnake3Score, col2X, colStartY+350);
-
     }
   }
 
@@ -245,6 +250,7 @@ function clickEventListener(event) {
   if (y > progBar.y && y < progBar.y + progBar.height
              && x > progBar.x && x < progBar.x + progBar.width) {
              gameRealtime = false;
+             updatingByLogic = true;
              var clickedGameState = (x- startX)*config.game_width/blockSize;
              if(clickedGameState >= startedViewingGamestate)
              {
@@ -348,23 +354,6 @@ function updateGameState()
   if(true)
   {
     realtimeGamestate++;
-    gameState =
-    {
-      ref: 0,
-      state: 0,
-      apple: appleX+" "+appleY,
-      obstacle0: "",
-      obstacle1: "",
-      obstacle2: "",
-      snake0: "",
-      snake1: "",
-      snake2: "",
-      snake3: "",
-      snake0Score: 0,
-      snake1Score: 0,
-      snake2Score: 0,
-      snake3Score: 0
-    };
     appleX++;
     if(appleX > 49)
     {
@@ -391,6 +380,23 @@ function updateGameState()
       }
     }
     gameColours.apple = 'rgb('+appleCol.r+','+appleCol.g+','+appleCol.b+')';
+    gameStateArr[realtimeGamestate] =
+    {
+      ref: 0,
+      state: 0,
+      apple: appleX+" "+appleY,
+      obstacle0: "",
+      obstacle1: "",
+      obstacle2: "",
+      snake0: "",
+      snake1: "",
+      snake2: "",
+      snake3: "",
+      snake0Score: 0,
+      snake1Score: 0,
+      snake2Score: 0,
+      snake3Score: 0
+    };
   }
   if(gameRewind)
   {
@@ -419,7 +425,7 @@ function updateGameState()
   {
     currentGamestate++;
   }
-
+  gameState = gameStateArr[currentGamestate];
 }
 
 function refreshLeaderboardAndDivisions()
@@ -477,8 +483,15 @@ function App() {
     useEffect(() => {
 
       if (viewerRef.current) {
-
+        if(!addedClickEvent)
+                {
+                  viewerRef.current.addEventListener('click', clickEventListener, false);
+                  viewerX = viewerRef.current.offsetLeft + viewerRef.current.clientLeft;
+                  viewerY = viewerRef.current.offsetTop + viewerRef.current.clientTop;
+                  addedClickEvent = true;
+                }
         const renderCtx = viewerRef.current.getContext('2d');
+
 
         if (renderCtx) {
           setContext(renderCtx);
@@ -539,6 +552,9 @@ function App() {
   return (
     <div className="row">
         <div className="column left">
+        <div id="header" className="alt">
+                <a className="logo" href="/"><strong>Snake AI Competition</strong> 2020</a>
+            </div>
           <h2>Division</h2>
           <select>
           {divisions.map(division => (
@@ -570,11 +586,12 @@ function App() {
           <div>
 
           <div id="viewerTimeControls" className="buttons">
-            <button onClick={() => {setPaused(false);setRewind(prevState => !prevState);setFfwd(false); setRealtime(false);}}><i className="material-icons">fast_rewind</i></button>
+            <button onClick={() => {setPaused(false);setRewind(false);setFfwd(false); setRealtime(false);currentGamestate = startedViewingGamestate}}><i className="material-icons">skip_previous</i></button>
+            <button onClick={() => {setPaused(false);setRewind(true);setFfwd(false); setRealtime(false);}}><i className="material-icons">fast_rewind</i></button>
             <button onClick={() => {setPaused(prevState => !prevState);setRewind(false);setFfwd(false); setRealtime(false);}}><i className="material-icons">{paused ? "play_circle_outline" : "pause_circle_outline"}</i></button>
-            <button onClick={() => {setPaused(false);setRewind(false);setFfwd(prevState => !prevState); setRealtime(false);}}><i className="material-icons">fast_forward</i></button>
+            <button onClick={() => {setPaused(false);setRewind(false);setFfwd(true); setRealtime(false);}}><i className="material-icons">fast_forward</i></button>
             {realtime ? null : <button onClick={() => {setPaused(false);setRewind(false);setFfwd(false); setRealtime(true);currentGamestate = realtimeGamestate}}>
-            {<i className="material-icons">fast_forward</i>}
+            {<i className="material-icons">skip_next</i>}
             </button>}
           </div>
           <div id="viewerLookControls" className="buttons">
